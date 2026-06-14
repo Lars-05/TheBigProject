03d25cdf86f472e31bb8b63ba545de4ec5da71c8
Lars <lars.versteegen6@gmail.com>
2026-06-14 22:59:45 +0200

    Added Pasive sanity drain

--- Assets/Scripts/SanitySystem/SanityDisplayer.cs
+++ Assets/Scripts/SanitySystem/SanityDisplayer.cs
@@ -8,6 +8,8 @@ public class SanityDisplayer : MonoBehaviour
     [SerializeField] private Slider _sanitySlider;
     [SerializeField] private float _sliderChangeDuration;
     private Tween _sliderTween;
+
+ 
     public void UpdateSlider(float pSanity)
     {
         _sliderTween?.Kill();
--- Assets/Scripts/SanitySystem/SanityManager.cs
+++ Assets/Scripts/SanitySystem/SanityManager.cs
@@ -1,25 +1,53 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SanityManager : MonoBehaviour
 {
     public delegate void SanityDisplayerEvent(int amount);
+
     public static SanityDisplayerEvent GainSanity;
     public static SanityDisplayerEvent LoseSanity;
-    
+
     [SerializeField] private SanityDisplayer _sanityDisplayer;
+    [SerializeField] private int sanityDrainPS = 1;
+
     private int sanity;
-    
-    
+    private Coroutine drainRoutine;
+
     private void OnEnable()
     {
         GainSanity += IncreaseSanity;
         LoseSanity += DecreaseSanity;
+
+        drainRoutine = StartCoroutine(DrainSanity());
     }
     
+    private void Start()
+    {
+        sanity = Mathf.RoundToInt(_sanityDisplayer.GetComponent<SanityDisplayer>()
+            .GetComponentInChildren<Slider>().maxValue);
+    }
     private void OnDisable()
     {
         GainSanity -= IncreaseSanity;
         LoseSanity -= DecreaseSanity;
+
+        if (drainRoutine != null)
+        {
+            StopCoroutine(drainRoutine);
+            drainRoutine = null;
+        }
+    }
+
+    private IEnumerator DrainSanity()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            DecreaseSanity(sanityDrainPS);
+        }
     }
 
     public void IncreaseSanity(int amount)
@@ -31,7 +59,7 @@ public class SanityManager : MonoBehaviour
     public void DecreaseSanity(int amount)
     {
         sanity -= amount;
+        sanity = Mathf.Max(0, sanity);
         _sanityDisplayer.UpdateSlider(sanity);
     }
-    
 }
