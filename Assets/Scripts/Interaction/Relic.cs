eb7869c7012eca1fa3d76eab5a0d9e41ff3c6b83
Lars <lars.versteegen6@gmail.com>
2026-06-23 12:53:06 +0200

    Small Bug Fixes

--- Assets/Scripts/Interaction/Relic.cs
+++ Assets/Scripts/Interaction/Relic.cs
@@ -1,58 +1,93 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Relic : MonoBehaviour, IInteractable 
+public class Relic : MonoBehaviour, IInteractable
 {
-    [SerializeField] private Material _defaultMaterial;
     [SerializeField] private Material _selectedMaterial;
     [SerializeField] private GameObject _rotateObject;
     [SerializeField] private MeshRenderer _renderer;
-    
-    private Material[] _selectedMaterials;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _interactionDistance;
+
     private Material[] _defaultMaterials;
+    private Material[] _selectedMaterials;
+
+    private bool _collected;
 
     public static UnityEvent<int> OnRelicAdded = new();
     private static int _relicCount;
-    
+
     private void Awake()
     {
-        _defaultMaterials = new Material[1];
-        _selectedMaterials = new Material[2];
-        
-        _defaultMaterials[0] = this.gameObject.GetComponent<MeshRenderer>().material;
-        _selectedMaterials [0] = this.gameObject.GetComponent<MeshRenderer>().material;
-        _selectedMaterials[1] = _selectedMaterial;
+        if (_renderer == null)
+            _renderer = GetComponent<MeshRenderer>();
 
+        if (_renderer == null)
+        {
+            Debug.LogError($"No MeshRenderer found on {name}");
+            return;
+        }
+        
+        _defaultMaterials = _renderer.materials;
+        
+        _selectedMaterials = new Material[_defaultMaterials.Length + 1];
+
+        for (int i = 0; i < _defaultMaterials.Length; i++)
+            _selectedMaterials[i] = _defaultMaterials[i];
+
+        _selectedMaterials[^1] = _selectedMaterial;
+    }
+
+    private bool PlayerInRange()
+    {
+        return Vector3.Distance(_player.transform.position, _renderer.bounds.center) < _interactionDistance;
     }
 
     public void OnHoverEnter()
     {
-        if (_renderer == null)
-            return;
-        _renderer.materials = _selectedMaterials;
+        if (_renderer != null && PlayerInRange())
+            _renderer.materials = _selectedMaterials;
     }
 
     public void OnHoverExit()
     {
-        if (_renderer == null)
-            return;
-        _renderer.materials = _defaultMaterials;
+        if (_renderer != null)
+            _renderer.materials = _defaultMaterials;
     }
 
     public void OnHoverStay()
     {
-        if (_renderer == null)
-            return;
+        if(PlayerInRange())
+        {
+            OnHoverEnter();
+        }
     }
-    
+
     public void OnInteract()
     {
+        if (!PlayerInRange())
+            return;
+            
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
         InputManager.Instance.SwitchActionMap("ObjectView");
-        _rotateObject.SetActive(true);
+
+        if (_rotateObject != null)
+            _rotateObject.SetActive(true);
         
-        _relicCount++;
-        OnRelicAdded.Invoke(_relicCount);
+        if (!_collected)
+        {
+            _collected = true;
+            _relicCount++;
+            OnRelicAdded.Invoke(_relicCount);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, _interactionDistance);
     }
 }
