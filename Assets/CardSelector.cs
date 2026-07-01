38544327d7114898c0985a3ab5dc60ee6a5c2ccd
Stan Oosterdijk <sj.oosterdijk@hotmail.com>
2026-07-01 12:47:12 +0200

    Hotfix

--- Assets/CardSelector.cs
+++ Assets/CardSelector.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -7,13 +9,15 @@ public class CardSelector : MonoBehaviour
 {
     [SerializeField] private int _selectedCardIndex;
     [SerializeField] private GameObject _player;
-    private IUsable[] _usables => transform.parent.GetComponentsInChildren<IUsable>();
+    private List<IUsable> _usables;
     private IDisposable _sub;
     private IDisposable _sub2;
     private float _scrollValue;
+    private bool _using;
 
     private void Start()
     {
+        _usables = transform.parent.GetComponentsInChildren<IUsable>().ToList();
         RefreshCards();
     }
     
@@ -31,12 +35,21 @@ public class CardSelector : MonoBehaviour
     
     private void UseCard(InputAction.CallbackContext _)
     {
+        if (_using)
+            return;
+        
+        _using = true;
+        Invoke(nameof(Wait), 10.25f);
+        
         _usables[_selectedCardIndex].Use(_player);
-        RefreshCards();
+        _usables.RemoveAt(_selectedCardIndex);
     }
     
     private void NextCard(InputAction.CallbackContext context)
     {
+        if (_using)
+            return;
+        
         _scrollValue += context.ReadValue<float>();
 
         if (_scrollValue >= 2)
@@ -46,7 +59,7 @@ public class CardSelector : MonoBehaviour
             transform.GetChild(_selectedCardIndex).GetComponent<Outline>().enabled = false;
             _selectedCardIndex++;
             
-            if(_selectedCardIndex >= _usables.Length)
+            if(_selectedCardIndex >= _usables.Count)
                 _selectedCardIndex = 0;
             
             transform.GetChild(_selectedCardIndex).GetComponent<Outline>().enabled = true;
@@ -60,7 +73,7 @@ public class CardSelector : MonoBehaviour
             _selectedCardIndex--;
             
             if(_selectedCardIndex < 0)
-                _selectedCardIndex = _usables.Length-1;
+                _selectedCardIndex = _usables.Count-1;
             
             transform.GetChild(_selectedCardIndex).GetComponent<Outline>().enabled = true;
 
@@ -70,7 +83,12 @@ public class CardSelector : MonoBehaviour
     private void RefreshCards()
     {
         _selectedCardIndex = 0;
-
         transform.GetChild(_selectedCardIndex).GetComponent<Outline>().enabled = true;
+    }
+
+    private void Wait()
+    {
+        _using = false;
+        RefreshCards();
     }
 }
